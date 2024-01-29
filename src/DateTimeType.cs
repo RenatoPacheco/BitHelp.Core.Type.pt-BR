3cf3ec4f22a3d4b5f5d9725b67b6cbf31f5126a4
Renato Pacheco <renato.pacheco@adm.unip.br>
2024-01-29 15:55:38 -0300

    refactor: Adding DateTimeType

---
+++ src/DateTimeType.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using BitHelp.Core.Type.pt_BR.Helpers;
+using BitHelp.Core.Type.pt_BR.Resources;
+
+namespace BitHelp.Core.Type.pt_BR
+{
+    public struct DateTimeType
+        : IFormattable, IComparable, IConvertible,
+        IComparable<DateTimeType>, IEquatable<DateTimeType>
+    {
+        public DateTimeType(string input)
+        {
+            TryParse(input, out DateTimeType output);
+            this = output;
+        }
+
+        private string _value;
+        private bool _isValid;
+
+        public static implicit operator string(DateTimeType input) => input.ToString();
+        public static implicit operator DateTimeType(string input) => new DateTimeType(input);
+
+        public static DateTimeType Parse(string input)
+        {
+            if (TryParse(input, out DateTimeType result))
+            {
+                return result;
+            }
+            else
+            {
+                if (input == null)
+                    throw new ArgumentException(
+                        nameof(input), Resource.ItCannotBeNull);
+                else
+                    throw new ArgumentException(
+                        nameof(input), Resource.ItIsNotInAValidFormat);
+            }
+        }
+
+        public static bool TryParse(string input, out DateTimeType output)
+        {
+            input = input?.Trim() ?? string.Empty;
+            if (!string.IsNullOrEmpty(input))
+            {
+                string value = input;
+                string pattern = @"^\d{1,2}[\/\-]?\d{1,2}[\/\-]?\d{4}";
+                IFormatProvider provider = CultureInfo.GetCultureInfo("pt-BR");
+                if (Regex.IsMatch(value, pattern, RegexOptions.None, AppSettings.RegEx.TimeOut))
+                {
+                    pattern = @"[\/\-]+";
+                    value = Regex.Replace(value, pattern, "/", RegexOptions.None, AppSettings.RegEx.TimeOut);
+                    
+                    if(DateTime.TryParse(value, provider,
+                        DateTimeStyles.None, out DateTime v))
+                    {
+                        output = new DateTimeType
+                        {
+                            _value = v.ToString("dd/MM/yyyy HH:mm:ss", provider),
+                            _isValid = true
+                        };
+                        return true;
+                    }
+                }
+            }
+            output = new DateTimeType
+            {
+                _value = input,
+                _isValid = false
+            };
+            return false;
+        }
+
+        public bool IsValid() => _isValid;
+
+        public override string ToString()
+        {
+            return ToString(null, null);
+        }
+
+        public string ToString(string format)
+        {
+            return ToString(format, null);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return _value;
+        }
+
+        public override int GetHashCode()
+        {
+            return $"{_value}:{GetType()}".GetHashCode();
+        }
+
+        public bool Equals(DateTimeType other)
+        {
+            return GetHashCode() == other.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DateTimeType phone && Equals(phone);
+        }
+
+        public int CompareTo(DateTimeType other)
+        {
+            return _value.CompareTo(other._value);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is DateTimeType phone)
+            {
+                return CompareTo(phone);
+            }
+
+            throw new ArgumentException(
+                nameof(obj), Resource.ItIsNotAValidType);
+        }
+
+        public static bool operator ==(DateTimeType left, DateTimeType right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(DateTimeType left, DateTimeType right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator >(DateTimeType left, DateTimeType right)
+        {
+            return left.CompareTo(right) == 1;
+        }
+
+        public static bool operator <(DateTimeType left, DateTimeType right)
+        {
+            return left.CompareTo(right) == -1;
+        }
+
+        #region IConvertible implementation
+
+        public TypeCode GetTypeCode()
+        {
+            return TypeCode.String;
+        }
+
+        /// <internalonly/>
+        string IConvertible.ToString(IFormatProvider provider)
+        {
+            return _value;
+        }
+
+        /// <internalonly/>
+        bool IConvertible.ToBoolean(IFormatProvider provider)
+        {
+            return Convert.ToBoolean(_value);
+        }
+
+        /// <internalonly/>
+        char IConvertible.ToChar(IFormatProvider provider)
+        {
+            return Convert.ToChar(_value);
+        }
+
+        /// <internalonly/>
+        sbyte IConvertible.ToSByte(IFormatProvider provider)
+        {
+            return Convert.ToSByte(_value);
+        }
+
+        /// <internalonly/>
+        byte IConvertible.ToByte(IFormatProvider provider)
+        {
+            return Convert.ToByte(_value);
+        }
+
+        /// <internalonly/>
+        short IConvertible.ToInt16(IFormatProvider provider)
+        {
+            return Convert.ToInt16(_value);
+        }
+
+        /// <internalonly/>
+        ushort IConvertible.ToUInt16(IFormatProvider provider)
+        {
+            return Convert.ToUInt16(_value);
+        }
+
+        /// <internalonly/>
+        int IConvertible.ToInt32(IFormatProvider provider)
+        {
+            return Convert.ToInt32(_value);
+        }
+
+        /// <internalonly/>
+        uint IConvertible.ToUInt32(IFormatProvider provider)
+        {
+            return Convert.ToUInt32(_value);
+        }
+
+        /// <internalonly/>
+        long IConvertible.ToInt64(IFormatProvider provider)
+        {
+            return Convert.ToInt64(_value);
+        }
+
+        /// <internalonly/>
+        ulong IConvertible.ToUInt64(IFormatProvider provider)
+        {
+            return Convert.ToUInt64(_value);
+        }
+
+        /// <internalonly/>
+        float IConvertible.ToSingle(IFormatProvider provider)
+        {
+            return Convert.ToSingle(_value);
+        }
+
+        /// <internalonly/>
+        double IConvertible.ToDouble(IFormatProvider provider)
+        {
+            return Convert.ToDouble(_value);
+        }
+
+        /// <internalonly/>
+        decimal IConvertible.ToDecimal(IFormatProvider provider)
+        {
+            return Convert.ToDecimal(_value);
+        }
+
+        /// <internalonly/>
+        DateTime IConvertible.ToDateTime(IFormatProvider provider)
+        {
+            return Convert.ToDateTime(_value);
+        }
+
+        /// <internalonly/>
+        object IConvertible.ToType(System.Type type, IFormatProvider provider)
+        {
+            return Convert.ChangeType(this, type, provider);
+        }
+
+        #endregion
+    }
+}
---
+++ test/DateTimeTypeTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+
+namespace BitHelp.Core.Type.pt_BR.Test
+{
+    public class DateTimeTypeTest
+    {
+        [Theory]
+        [InlineData("31/12/2020 12:34:56", "31/12/2020 12:34:56")]
+        [InlineData("31-12-2020 12:34:56", "31/12/2020 12:34:56")]
+        [InlineData("1/1/2020 12:34:56", "01/01/2020 12:34:56")]
+        [InlineData("1/1/2020", "01/01/2020 00:00:00")]
+        [InlineData("1/1/2020 12:00", "01/01/2020 12:00:00")]
+        public void Check_format_is_valid(string input, string expected)
+        {
+            DateTimeType test = new(input);
+            Assert.Equal(expected, test.ToString());
+            Assert.True(test.IsValid());
+
+            test = DateTimeType.Parse(input);
+            Assert.Equal(expected, test.ToString());
+            Assert.True(test.IsValid());
+
+            test = input;
+            Assert.Equal(expected, test.ToString());
+            Assert.True(test.IsValid());
+
+            input = test;
+            Assert.Equal(expected, input);
+        }
+
+        [Theory]
+        [InlineData("31 12 2020")]
+        [InlineData("12/31/2020")]
+        [InlineData("31/12/20")]
+        public void Check_format_is_invalid(string input)
+        {
+            DateTimeType test = new(input);
+            Assert.Equal(input?.Trim() ?? string.Empty, test.ToString());
+            Assert.False(test.IsValid());
+
+            test = input;
+            Assert.Equal(input?.Trim() ?? string.Empty, test.ToString());
+            Assert.False(test.IsValid());
+        }
+
+        [Theory]
+        [InlineData("31 12 2020")]
+        [InlineData("12/31/2020")]
+        [InlineData("31/12/20")]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Check_parse_exception(string input)
+        {
+            Assert.Throws<ArgumentException>(() => DateTimeType.Parse(input));
+        }
+    }
+}
